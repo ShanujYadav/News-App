1b8299bfd3ea5794ffa9c95d210bba988664781d
Shanuj Yadav <92615408+ShanujYadav@users.noreply.github.com>
2023-05-02 17:11:34 +0530

    Create type.ts

---
+++ screens/utils/type.ts
@@ -0,0 +1,24 @@
+//@ts-nocheck
+import { NavigationProp,RouteProp } from "@react-navigation/native";
+import { Route } from "react-native";
+
+export type NewsData = {
+    title: string;
+    link: string;
+    keywords: string[];
+    creator: string;
+    video_url: string;
+    description: string;
+    content: string;
+    pubDate: string;
+    image_url: string;
+    source_id: string;
+    category: string[];
+    country: string;
+    language: string;
+
+};
+export type ComponentNavigationProps = {
+    navigation: NavigationProp<Route>;
+    route: RouteProp<Route>
+}
